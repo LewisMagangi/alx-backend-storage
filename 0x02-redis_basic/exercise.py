ddddfc76b205d721549df9febc585ec4601b1503
LewisMagangi <lewismomanyi34@gmail.com>
2024-10-28 10:37:13 +0000

    Fixing the private var syntax and working on the type anotations correctly

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,14 +2,15 @@
 
 import redis
 import uuid
+from typing import Union
+
 
 class Cache:
-   def __init__(self):
-      self.__redis = redis.Redis(host='localhost', port='6379', db=0)
+    def __init__(self):
+        self._redis = redis.Redis(host='localhost', port='6379', db=0)
+        self._redis.flushdb()
 
-      self.__redis.flushdb()
-
-   def store(self, data):
-      random_key = str(uuid.uuid4())
-      self.__redis.set(random_key, data)
-      return random_key
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return random_key

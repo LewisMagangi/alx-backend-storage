ff0bbdea8983f43e3bcf3583b9b65a10f0c4cfd2
LewisMagangi <lewismomanyi34@gmail.com>
2024-10-30 06:38:51 -0700

    making the script executable and adding a certain logic

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- script.sql
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
        id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
        email VARCHAR(255) NOT NULL UNIQUE,
        name VARCHAR(255) NOT NULL
